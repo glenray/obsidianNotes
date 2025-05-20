15db397eb66d5cde2044f157c2a675936d2bd8e4
glen <glen@glenpritchard.com>
2025-05-20 13:50:22 -0400

    Adds Notes Class

--- Notes.py
+++ Notes.py
@@ -1,7 +1,33 @@
 import json
+import os
 from pathlib import Path
 import frontmatter
-# from .Vaulttype import VaultType
+
+
+class Notes():
+	def __init__(self, paths, recursive=True):
+		self.recursive = recursive
+		self.paths = paths
+		self.notes = []
+		self.propTypes = Note.getVaultTypes(self.paths)
+		self.add_notes(self.paths, self.recursive)
+
+
+	def add_notes(self, paths, recursive = True):
+		if isinstance(paths, Path):
+			paths = [paths]
+		for path in paths:
+			assert path.exists(), f"file or folder does not exist: '{path}'"
+			if path.is_dir():
+				for root, _, files in os.walk(path):
+					for f_name in files:
+						pth_f = Path(root) / f_name
+						if Note._is_md_file(pth_f):
+							self.notes.append(Note(path=pth_f))
+					if not recursive:
+						break
+			elif Note._is_md_file(path):
+				self.notes.append(Note(path=path))
 
 
 class Note():
@@ -11,8 +37,8 @@ class Note():
 		self.vaultTypes = Note.getVaultTypes(self.path)
 		
 
-	@classmethod
-	def getVaultTypes(cls, path):
+	@staticmethod
+	def getVaultTypes(path):
 		typesFile = '.obsidian/types.json'
 		attempt = None
 		root = Path(path.root)
@@ -29,6 +55,12 @@ class Note():
 		else:
 			return None
 
+	@staticmethod
+	def _is_md_file(path: Path):
+		exist = path.exists()
+		is_md = path.suffix == ".md"
+		return exist and is_md
+
 
 	def to_string(self):
 		content = frontmatter.dumps(self.post, encoding='utf-8', sort_keys=False, width=0, allow_unicode=True)
@@ -40,14 +72,24 @@ class Note():
 		frontmatter.dump(self.post, fn, encoding='utf-8', sort_keys=False, allow_unicode=True)
 
 
-	def has_key(self, key):
-		return key in self.post.metadata
+	def has_meta(self, key, value=None):
+		pm = self.post.metadata
+		# does the property key exist
+		if value == None:
+			return key in pm
+		
+		# does the property key match a value
+		if type(pm[key]) is list:
+			return value in pm[key]
+		else:
+			return pm[key] == value
+
 
 
 	def add(self, k, v=None, overwrite=False):
 		pm = self.post.metadata
 		# adding a new property
-		if not self.has_key(k):
+		if not self.has_meta(k):
 			pm[k] = v
 		# modify existing list property
 		else:
@@ -67,7 +109,7 @@ class Note():
 		'''
 		Remove a property completely
 		'''
-		if self.has_key(k):
+		if self.has_meta(k):
 			del self.post.metadata[k]
 
 
@@ -76,16 +118,10 @@ class Note():
 		If the key value is a list, the value will be removed from that list. All other property types will be set to None
 		'''
 		pm = self.post.metadata
-		if self.has_key(k):
+		if self.has_meta(k):
 			if type(pm[k]) is list:
 				# remove value from list of
 				if v in pm[k]:
 					pm[k].remove(v)
 			else:
 				pm[k] = None
-
-
-if __name__ == "__main__":
-	secondNotePath='/home/glen/Documents/obsidian/obNoteTest/subdir/Second Note.md'
-	n = obNote(secondNotePath)
-	breakpoint()
